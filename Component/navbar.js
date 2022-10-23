11f3f768895d9aea92a1cc9149488691e64c0fe3
sgovind158 <govindsahu2842@gmail.com>
2022-10-23 14:09:11 +0530

    project done

---
+++ Component/navbar.js
@@ -0,0 +1,13 @@
+function navbar(){
+    return `
+    <div id="nav">
+    <h1><a href="index.html">HomePage</a></h1>
+    <h1><a href="cat.html">CatPage</a></h1>
+    <h1><a href="search.html">SearchPage</a></h1>
+  </div>
+    
+    `;
+};
+
+
+export { navbar };
